63efa28bba05f5afcf7748816fc0fb8bf0786bdc
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-07 08:44:33 -0700

    TDD API Utility

--- test/api-test.js
+++ test/api-test.js
@@ -1,4 +1,5 @@
 const test = QUnit.test;
+QUnit.module('api');
 
 api.storage = sessionStorage;
 
@@ -13,20 +14,29 @@ const api = {
     signUp(user) {
         // initialize any use data
         //stringify and save to api storage
+        const json = JSON.stringify(user);
         //remove completed key
+        api.storage.setItem('user', json);
     },
     getUser() {
         //return the user from api.storage
+        const json = api.storage.getItem('user');
+        if(!json) return null;
+        const user = JSON.parse(json);
+        return user;
     }
-});
-export defaul api;
+};
 
-test(signUp sets user and returns on getUser) => {
+test('signUp sets user and returns on getUser', (assert) => {
     const user = {
-
+        name: 'Henry',
+        animal: 'chicken',
+        bells: 100,
+        friendship: 5
     };
     //act
-    
+    api.signUp(user);
+    const result = api.getUser();
     //assert
     assert.deepEqual(result, user);
 });
