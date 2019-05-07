317011e4a4de5c51834a7ba0388ddc7f29c3d86c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-07 08:50:19 -0700

    exported api

---
+++ src/api.js
@@ -0,0 +1,20 @@
+const api = {
+    // used by testing to control where API saves data
+    storage: localStorage,
+    // Methods for reading and saving data
+    signUp(user) {
+        // initialize any use data
+        //stringify and save to api storage
+        const json = JSON.stringify(user);
+        //remove completed key
+        api.storage.setItem('user', json);
+    },
+    getUser() {
+        //return the user from api.storage
+        const json = api.storage.getItem('user');
+        if(!json) return null;
+        const user = JSON.parse(json);
+        return user;
+    }
+};
+export default api;
--- test/api-test.js
+++ test/api-test.js
@@ -1,4 +1,6 @@
+import api from '../src/api.js';
 const test = QUnit.test;
+
 QUnit.module('api');
 
 api.storage = sessionStorage;
@@ -6,26 +8,6 @@ api.storage = sessionStorage;
 QUnit.testStart(() => {
     sessionStorage.clear();
 });
-
-const api = {
-    // used by testing to control where API saves data
-    storage: localStorage,
-    // Methods for reading and saving data
-    signUp(user) {
-        // initialize any use data
-        //stringify and save to api storage
-        const json = JSON.stringify(user);
-        //remove completed key
-        api.storage.setItem('user', json);
-    },
-    getUser() {
-        //return the user from api.storage
-        const json = api.storage.getItem('user');
-        if(!json) return null;
-        const user = JSON.parse(json);
-        return user;
-    }
-};
 
 test('signUp sets user and returns on getUser', (assert) => {
     const user = {
