7945434261bee447a4fc09aeabbdcaa4d18899d7
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-06 17:27:38 -0700

    TDD Make User Function

--- test/tests.js
+++ test/tests.js
@@ -1,12 +1,32 @@
 const test = QUnit.test;
+function makeUser(formData) {
+    const user = {
+        name: formData.get('name'),
+        animal: formData.get('animal'),
+        bells: 100,
+        friendship: 5
+    };
+    return user;
+}
 
-test('time to test a function', function(assert) {
+test('create user object from form data', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const formData = new FormData();
+    //set values in strings
+    formData.set('name', 'Henry');
+    formData.set('animal', 'chicken');
+
+    const expected = {
+        name: 'Henry',
+        animal: 'chicken',
+        bells: 100,
+        friendship: 5,
+    };
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const user = makeUser(formData);
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(user, expected);
 });
