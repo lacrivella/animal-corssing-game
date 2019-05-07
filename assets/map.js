7c49c43af787732b5f7634a67f3d291587558256
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-05-07 13:03:46 -0700

    populated user profile

--- assets/map.js
+++
@@ -1,7 +0,0 @@
-import api from '../src/api.js';
-
-// reference DOM
-
-//load profile with user
-
-//copy data from object to DOM properties
---
+++ src/map.js
@@ -0,0 +1,20 @@
+import api from './api.js';
+
+// reference DOM
+const name = document.getElementById('name');
+const animal = document.getElementById('animal');
+const bells = document.getElementById('bells');
+const friendship = document.getElementById('friendship');
+
+//load profile with user
+const user = api.getUser();
+
+if(!user) {
+    window.location = './';
+}
+
+//copy data from object to DOM properties
+name.textContent = user.name;
+animal.src = './assets/avatars/' + user.animal + '.png';
+bells.textContent = user.bells;
+friendship.textContent = user.friendship;
